b8d61675ee1d46f63bff3067a947db3ec60ef54f
jirosmokes <dustinrafe@gmail.com>
2024-10-25 09:37:08 +0800

    Updated: updated remove car function, each car has remove btn for user's choice

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 const inputText = document.querySelector('input');
-
-const addCarBtn = document.querySelector('.add-car-btn');
-
 const cardContainer = document.querySelector('.card-container');
 
+// Add a car function
+const addCarBtn = document.querySelector('.add-car-btn');
 addCarBtn.addEventListener("click",() => {
-    const carInput = inputText.value;
+    const carInput = inputText.value.trim();
 
+    if(cardContainer.children.length >= 12){
+        alert("You've reached the maximum number of vehicles.");
+        return;
+    }
+
+    if(carInput == ''){
+        alert('Enter a value');
+        return;
+    }
     const carCard = document.createElement('div');
     carCard.classList.add('card');
 
-    let carHeader = document.createElement('span');
+    const imageHolder = document.createElement('img');
+    imageHolder.src = "https://placehold.co/100x100";
+
+    const carHeader = document.createElement('span');
     carHeader.textContent = carInput;
-    carCard.appendChild(carHeader);
+
+    carHeader.addEventListener("mouseover", () => {
+        carHeader.style.fontWeight = "bold";
+    })
+
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = `Remove`;
+    removeBtn.classList.add('remove-car-btn');
+
+    // Remove each card for user's choice
+    removeBtn.addEventListener("click",() => {
+        cardContainer.removeChild(carCard);
+    })
 
     cardContainer.appendChild(carCard);
-
+    
+    carCard.appendChild(imageHolder);
+    carCard.appendChild(carHeader);
+    carCard.appendChild(removeBtn);
     inputText.value = '';
 
 })
